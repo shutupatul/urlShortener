73c9b8684846350af0ae3c5cbf63eed63bfdb9e0
Atul Kanojia <heyatulonmail@gmail.com>
2025-03-22 12:38:39 +0530

    Before Server Side Rendering

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
 const express = require("express");
-
+const { connectToMongoDB } = require("./connect");
+const urlRoute = require("./routes/url");
+const { timeStamp } = require("console");
+const URL = require("./models/url");
 const app = express();
 const PORT = 8001;
 
-app.listen(PORT, () => console.log(`Server statrted at PORT: ${PORT}`));
+connectToMongoDB("mongodb://localhost:27017/short-url").then(() =>
+  console.log("MongoDB Connected")
+);
 
+app.use(express.json());
+
+app.use("/url", urlRoute);
+
+app.get("/test", (req, res) => {
+  res.end(`<h1>Hey from Atul's Server</h1>`);
+});
+
+app.get("/:shortId", async (req, res) => {
+  const shortId = req.params.shortId;
+  const entry = await URL.findOneAndUpdate(
+    {
+      shortId,
+    },
+    {
+      $push: {
+        visitHistory: {
+          timeStamp: Date.now(),
+        },
+      },
+    }
+  );
+  res.redirect(entry.redirectURL);
+});
+
+app.listen(PORT, () => console.log(`Server statrted at PORT: ${PORT}`));
